6680e3b8c27c0f6111659f4ecad0674718459edc
mogaitesheng <2530278940wc@gmail.com>
2024-09-08 10:55:50 +0800

    更改文件描述

---
+++ ServerSDK/mg_mysql.cpp
@@ -0,0 +1,1 @@
+#include "mg_mysql.h"
---
+++ ServerSDK/mg_socket.h
@@ -0,0 +1,52 @@
+#ifndef __MG_SOCKET_H__
+#define __MG_SOCKET_H__
+
+#include <unistd.h>
+#include <sys/socket.h>
+
+#include "mg_noncopyable.h"
+
+namespace mg
+{
+
+    enum SOCKER_TYPE
+    {
+        TCP_SOCKET = 1,
+        UDP_SOCKET = 2
+    };
+
+    enum DOMAIN_TYPE
+    {
+        IPV4_DOMAIN = 1,
+        IPV6_DOMAIN = 2
+    };
+};
+
+namespace mg
+{
+    class Socket : noncopyable
+    {
+    public:
+        Socket();
+
+        ~Socket();
+
+        /**
+         * @brief 返回socket对象管理的文件描述符
+         */
+        inline int fd() const { return socket_fd; }
+
+        /**
+         * @brief 设置套接口属性
+         *
+         * @param type 套接口SOCKER_TYPE枚举值
+         * @return true 设置成功 false 设置失败
+         */
+        bool setSocketType(int domain, int type);
+
+    private:
+        int socket_fd;
+    };
+};
+
+#endif //__MG_SOCKET_H__
