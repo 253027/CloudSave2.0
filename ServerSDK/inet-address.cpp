bd81dd7bcc9fe57728a1013c59809679f78e739e
mogaitesheng <2530278940wc@gmail.com>
2024-09-12 23:43:42 +0800

    添加ip地址存储类

---
+++ ServerSDK/inet-address.cpp
@@ -0,0 +1,71 @@
+#include "inet-address.h"
+
+mg::InternetAddress::InternetAddress(uint16_t port, bool isIpv6)
+{
+    if (isIpv6)
+    {
+        ::memset(&_address6, 0, sizeof(_address6));
+        _address6.sin6_family = AF_INET6;
+        _address6.sin6_addr = in6addr_any;
+        _address6.sin6_port = ::htons(port);
+        _ipv6 = true;
+    }
+    else
+    {
+        ::memset(&_address4, 0, sizeof(_address4));
+        _address4.sin_family = AF_INET;
+        _address4.sin_port = ::htons(port);
+        _address4.sin_addr.s_addr = ::htonl(INADDR_ANY);
+    }
+}
+
+mg::InternetAddress::InternetAddress(const sockaddr_in &address) : _address4(address) {}
+
+mg::InternetAddress::InternetAddress(const sockaddr_in6 &address) : _address6(address) {}
+
+mg::InternetAddress::InternetAddress(const std::string &ip, uint16_t port, bool isIpv6)
+{
+    if (isIpv6 || ip.find(":") != ip.npos)
+    {
+        ::memset(&_address6, 0, sizeof(_address6));
+        _address6.sin6_port = ::htons(port);
+        ::inet_pton(AF_INET6, ip.c_str(), &_address6.sin6_addr);
+        _ipv6 = true;
+    }
+    else
+    {
+        ::memset(&_address4, 0, sizeof(_address4));
+        _address4.sin_addr.s_addr = ::inet_addr(ip.c_str());
+        _address4.sin_port = ::htons(port);
+    }
+}
+
+std::string mg::InternetAddress::toIp() const
+{
+    return std::string();
+}
+
+std::string mg::InternetAddress::toIpPort() const
+{
+    char buf[64] = {0};
+    if (_ipv6)
+    {
+        ::inet_ntop(AF_INET6, &_address6.sin6_addr, buf, sizeof(buf));
+        int len = ::strlen(buf);
+        uint16_t port = ::ntohs(_address6.sin6_port);
+        snprintf(buf + len, sizeof(buf) - 1, ":%u", port);
+    }
+    else
+    {
+        ::inet_ntop(AF_INET, &_address4.sin_addr, buf, sizeof(buf));
+        int len = ::strlen(buf);
+        uint16_t port = ::ntohs(_address4.sin_port);
+        snprintf(buf + len, sizeof(buf) - 1, ":%u", port);
+    }
+    return buf;
+}
+
+uint16_t mg::InternetAddress::port() const
+{
+    return ::ntohs(_ipv6 ? _address6.sin6_port : _address4.sin_port);
+}
--- ServerSDK/inet-address.h
+++ ServerSDK/inet-address.h
@@ -1,11 +1,38 @@
 #ifndef __MG_INETADDRESS_H__
 #define __MG_INETADDRESS_H__
 
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string>
+#include <string.h>
+
 namespace mg
 {
     class InternetAddress
     {
-        ;
+    public:
+        explicit InternetAddress(uint16_t port = 0, bool isIpv6 = false);
+
+        explicit InternetAddress(const sockaddr_in &address);
+
+        explicit InternetAddress(const sockaddr_in6 &address);
+
+        InternetAddress(const std::string &ip, uint16_t port, bool isIpv6 = false);
+
+        std::string toIp() const;
+
+        std::string toIpPort() const;
+
+        uint16_t port() const;
+
+    private:
+        union
+        {
+            sockaddr_in _address4;
+            sockaddr_in6 _address6;
+        };
+
+        bool _ipv6 = false;
     };
 };
 
