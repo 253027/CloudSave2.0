a657fc4472df41fd33d24c5c42425ec43f6da1d5
mogaitesheng <2530278940wc@gmail.com>
2024-11-01 22:32:59 +0800

    Mysql类部分接口添加

--- ServerSDK/mysql.cpp
+++ ServerSDK/mysql.cpp
@@ -1,1 +1,56 @@
 #include "mysql.h"
+#include <sstream>
+#include <string.h>
+
+mg::Mysql::Mysql()
+{
+    ;
+}
+
+mg::Mysql::~Mysql()
+{
+    ;
+}
+
+std::string mg::Mysql::parseInsert(const std::string &name, mysql::DataField *column, char *data)
+{
+    using DATATYPE = mysql::DATATYPE;
+    using CALCTYPE = mysql::CALCTYPE;
+    int offset = 0;
+    std::ostringstream sql;
+    sql << "insert into `" << name << "` (";
+    std::string field, value;
+    for (mysql::DataField *p = column; p->name; p++)
+    {
+        if (!::strlen(p->name))
+            continue;
+        field += p->name, field += ", ";
+        int len = 0;
+        switch (p->type)
+        {
+        case DATATYPE::DB_STRING:
+        {
+            len = ::strlen(data + offset);
+            if (p->size != CALCTYPE::DB_AUTO)
+                len = std::min(len, p->size);
+            std::string buf(len * 2 + 3, '\'');
+            len = mysql_escape_string(buf.data() + 1, data + offset, len);
+            buf.resize(len + 2);
+            buf.back() = '\'';
+            value += buf;
+            break;
+        }
+        case DATATYPE::DB_INT32:
+        {
+            value += std::to_string(*((int *)(data + offset)));
+            break;
+        }
+        }
+        value += ", ";
+        offset += p->size ? p->size : len;
+    }
+    field = field.substr(0, field.size() - 2);
+    value = value.substr(0, value.size() - 2);
+    sql << field << ") values (" << value << ")";
+    return sql.str();
+}
--- ServerSDK/mysql.h
+++ ServerSDK/mysql.h
@@ -9,16 +9,84 @@
 #ifndef __MG_MYSQL_H__
 #define __MG_MYSQL_H__
 
+#include <string>
+#include "singleton.h"
 #include <mysql/mysql.h>
 
-class Mysql
+namespace mg
 {
-public:
-    Mysql();
+    /**
+     * 定义结构体：
+     *              struct name
+     *              {
+     *                  类型1 变量名1
+     *                  类型2 变量名2
+     *              }__attribute__((packed));
+     *
+     * 定义结构体描述和插入键值名：
+     *              DataField name[] =
+     *              {
+     *                {"数据列名1", "结构体成员类型", "成员大小所占字节数"},
+     *                 .......,
+     *                {nullptr, DB_INVALID, 0},      //最后一列必须以nullptr结束
+     *              };
+     *
+     * example:
+     *          struct test
+     *          {
+     *             char buf[33];
+     *              int a;
+     *          }__attribute__((packed));
+     *
+     *          mg::mysql::DataField field[] =
+     *          {
+     *              {"name",   mg::mysql::DATATYPE::DB_STRING, 33},
+     *              {"score",  mg::mysql::DATATYPE::DB_INT32, sizeof(int)},
+     *              { nullptr, mg::mysql::DATATYPE::DB_INVALID, 0},
+     *          };
+     */
+    namespace mysql
+    {
+        enum DATATYPE
+        {
+            DB_STRING = 1, // C风格字符串数据
+            DB_CHAR,       // 单字节数据
+            DB_INT32,      // 32位整型数据
+            DB_BIN,        // 二进制数据
+            DB_INVALID     // 无效位
+        };
 
-    ~Mysql();
+        enum CALCTYPE
+        {
+            DB_AUTO = 0,
+        };
 
-private:
+        struct DataField
+        {
+            char *name;    // 数据名
+            DATATYPE type; // 数据类型
+            int size;      // 数据大小(上限)
+        };
+    };
+
+    class Mysql : public Singleton<Mysql>
+    {
+    public:
+        Mysql();
+
+        ~Mysql();
+
+    public:
+        /**
+         * @brief 将要存入的数据结构转换成插入语句
+         * @param name 插入的表名
+         * @param column 数据结构描述列
+         * @return 生成sql语句
+         */
+        std::string parseInsert(const std::string &name, mysql::DataField *column, char *data);
+
+        MYSQL *handle;
+    };
 };
 
 #endif //__MG_MYSQL_H__
