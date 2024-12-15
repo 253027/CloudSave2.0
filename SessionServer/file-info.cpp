5fa75e0c97e0ac311e0f73c6a27ad75a4dfaf163
mogaitesheng <2530278940wc@gmail.com>
2024-12-15 21:31:21 +0800

    部分接口增加

--- SessionServer/file-info.cpp
+++ SessionServer/file-info.cpp
@@ -1,14 +1,54 @@
 #include "file-info.h"
+#include "../src/log.h"
 
 #include <unistd.h>
+#include <fcntl.h>
 
-FileInfo::FileInfo(const std::string &name, const std::string &hash, uint32_t size)
-    : _name(name), _fileHash(hash), _size(size)
+static const uint16_t initialChunkSize = 8192; // 8M bytes
+
+FileInfo::FileInfo(const std::string &name, const std::string &hash, uint32_t size, FILEMODE mode)
+    : _name(name), _fileHash(hash), _chunkPerSize(initialChunkSize), _size(size)
 {
-    ;
+    switch (mode)
+    {
+    case FILEMODE::READ:
+    {
+        this->_fd = ::open(name.c_str(), O_RDONLY);
+        break;
+    }
+    case FILEMODE::WRITE:
+    {
+        this->_fd = ::open(name.c_str(), O_WRONLY | O_CREAT);
+        break;
+    }
+    }
+    this->_nums = (this->_size + _chunkPerSize - 1) / _chunkPerSize;
 }
 
 FileInfo::~FileInfo()
 {
-    ;
+    TEMP_FAILURE_RETRY(::close(this->_fd));
+}
+
+int32_t FileInfo::write(int16_t chunkIndex, const std::string &data)
+{
+    if (chunkIndex >= this->_nums)
+    {
+        LOG_DEBUG("{} invalid chunkindex maxsize:{} input-size:{}", this->_name, this->_nums, chunkIndex);
+        return 0;
+    }
+    int ret = ::pwrite(this->_fd, data.data(), data.size(), this->_chunkPerSize * std::max(0, chunkIndex - 1));
+    LOG_DEBUG("{} index: {} wirte: {}", this->_name, chunkIndex, ret);
+    return ret;
+}
+
+bool FileInfo::exist(std::string &name)
+{
+    return ::access(name.c_str(), F_OK) == 0;
+}
+
+void FileInfo::setChunkSize(uint16_t size)
+{
+    this->_chunkPerSize = size;
+    this->_nums = (this->_size + size - 1) / size;
 }
--- SessionServer/file-info.h
+++ SessionServer/file-info.h
@@ -9,17 +9,40 @@
 class FileInfo
 {
 public:
-    FileInfo(const std::string &name, const std::string &hash, uint32_t size);
+    enum FILEMODE
+    {
+        READ = 1,  // 读
+        WRITE = 2, // 写
+    };
+
+    FileInfo(const std::string &name, const std::string &hash, uint32_t size, FILEMODE mode);
 
     ~FileInfo();
 
+    static bool exist(std::string &name);
+
     inline const std::string &getName() { return this->_name; };
+
+    /**
+     * @param 设置文件分块大小
+     */
+    void setChunkSize(uint16_t size);
+
+    /**
+     * @brief 写入文件
+     *
+     * @param chunkIndex 写入第几个文件分块
+     * @param data 写入数据
+     * @return 本次写入文件数据字节
+     */
+    int32_t write(int16_t chunkIndex, const std::string &data);
 
 private:
     int _fd;                                             // 写入文件的文件描述符
     std::string _name;                                   // 文件名（用户文件名）
     std::string _fileHash;                               // 文件hash值
     uint16_t _nums;                                      // 文件分块数
+    uint16_t _chunkPerSize;                              // 分块文件大小
     uint32_t _size;                                      // 文件总大小
     std::unordered_map<int16_t, std::string> _chunkHash; // 分块文件的Hash值
     std::unordered_map<int16_t, uint32_t> _chunkSize;    // 分块文件目前的大小
