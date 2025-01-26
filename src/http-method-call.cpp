9747cb582041422816adbdcbcef25cd107134f9d
mogaitesheng <2530278940wc@gmail.com>
2025-01-26 23:14:24 +0800

    支持正则表达式匹配

--- src/http-method-call.cpp
+++ src/http-method-call.cpp
@@ -8,6 +8,14 @@ bool mg::HttpMethodCall::regist(const std::string &name, const std::string &path
     return true;
 }
 
+bool mg::HttpMethodCall::regist2(const std::string &name, const std::string &path, HttpHandler handler)
+{
+    if (!this->basic_type.count(name))
+        return false;
+    this->_regexFunction[name].emplace_back(std::regex(path), std::move(handler));
+    return true;
+}
+
 bool mg::HttpMethodCall::exec(const HttpRequest &request)
 {
     return this->exec(request.method(), request.path(), request);
@@ -16,12 +24,23 @@ bool mg::HttpMethodCall::exec(const HttpRequest &request)
 bool mg::HttpMethodCall::exec(const std::string &name, const std::string &path, const HttpRequest &request)
 {
     auto it_name = this->_functions.find(name);
-    if (it_name == this->_functions.end())
-        return false;
+    if (it_name != this->_functions.end())
+    {
+        auto it_path = it_name->second.find(path);
+        if (it_path != it_name->second.end())
+            return it_path->second(request);
+    }
 
-    auto it_path = it_name->second.find(path);
-    if (it_path == it_name->second.end())
-        return false;
+    auto it_regex = this->_regexFunction.find(name);
+    if (it_regex != this->_regexFunction.end())
+    {
+        auto &functions = it_regex->second;
+        for (auto &x : functions)
+        {
+            if (std::regex_match(path, x.first))
+                return x.second(request);
+        }
+    }
 
-    return it_path->second(request);
+    return false;
 }
--- src/http-method-call.h
+++ src/http-method-call.h
@@ -10,6 +10,7 @@
 #include <unordered_set>
 #include <string>
 #include <tuple>
+#include <regex>
 
 namespace mg
 {
@@ -20,12 +21,15 @@ namespace mg
     public:
         bool regist(const std::string &name, const std::string &path, HttpHandler handler);
 
+        bool regist2(const std::string &name, const std::string &path, HttpHandler handler);
+
         bool exec(const HttpRequest &request);
 
         bool exec(const std::string &name, const std::string &path, const HttpRequest &request);
 
     private:
         std::unordered_map<std::string, std::unordered_map<std::string, HttpHandler>> _functions;
+        std::unordered_map<std::string, std::vector<std::pair<std::regex, HttpHandler>>> _regexFunction;
 
         const std::unordered_set<std::string> basic_type =
             {
