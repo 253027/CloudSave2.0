6167934d2ae48f3724f3f08b17d8dd27f4e6c98d
mogaitesheng <2530278940wc@gmail.com>
2025-02-09 18:30:11 +0800

    协议基类提交

---
+++ protocal/imPduBase.cpp
@@ -0,0 +1,52 @@
+#include "imPduBase.h"
+
+#include <string.h>
+
+PduMessage::PduMessage()
+{
+    ::memset(&this->_head, 0, sizeof(this->_head));
+}
+
+void PduMessage::setVersion(uint16_t version)
+{
+    this->_head.version = version;
+}
+
+void PduMessage::setFlag(uint16_t flag)
+{
+    this->_head.flag = flag;
+}
+
+void PduMessage::setServiceId(uint16_t id)
+{
+    this->_head.service_id = id;
+}
+
+void PduMessage::setCommandId(uint16_t id)
+{
+    this->_head.command_id = id;
+}
+
+void PduMessage::setSequenceNumber(uint16_t num)
+{
+    this->_head.sequence_number = num;
+}
+
+void PduMessage::setPBMessage()
+{
+    ;
+}
+
+std::string PduMessage::dump()
+{
+    mg::Buffer headBuffer;
+    headBuffer.appendInt32(this->_head.length);
+    headBuffer.appendUInt16(this->_head.version);
+    headBuffer.appendUInt16(this->_head.flag);
+    headBuffer.appendUInt16(this->_head.service_id);
+    headBuffer.appendUInt16(this->_head.command_id);
+    headBuffer.appendUInt16(this->_head.sequence_number);
+    headBuffer.appendUInt16(this->_head.reversed);
+    headBuffer.append(this->_buffer.retrieveAllAsString());
+    return headBuffer.retrieveAllAsString();
+}
---
+++ protocal/imPduBase.h
@@ -0,0 +1,43 @@
+#ifndef __IM_PDU_BASE_H__
+#define __IM_PDU_BASE_H__
+
+#include "../src/buffer.h"
+
+#include <cstdint>
+
+struct PduHeader
+{
+    uint32_t length;
+    uint16_t version;
+    uint16_t flag;
+    uint16_t service_id;
+    uint16_t command_id;
+    uint16_t sequence_number;
+    uint16_t reversed;
+};
+
+class PduMessage
+{
+public:
+    PduMessage();
+
+    void setVersion(uint16_t version);
+
+    void setFlag(uint16_t flag);
+
+    void setServiceId(uint16_t id);
+
+    void setCommandId(uint16_t id);
+
+    void setSequenceNumber(uint16_t num);
+
+    void setPBMessage();
+
+    std::string dump();
+
+private:
+    PduHeader _head;
+    mg::Buffer _buffer;
+};
+
+#endif //__IM_PDU_BASE_H__
