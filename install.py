87122a818d43174de2c3d1849ede3aff228b51ec
mogaitesheng <2530278940wc@gmail.com>
2025-02-09 21:13:13 +0800

    protobuf安装

---
+++ install.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+import os
+import tarfile
+import zipfile
+import subprocess
+import sys
+from typing import List
+
+current_path = os.path.dirname(os.path.realpath(__file__))
+
+def run_command(cmd: list[str]):
+    print(f"\033[32m{' '.join(cmd)}\033[0m")
+    result = subprocess.run(cmd)
+    if result.returncode != 0:
+        sys.exit(1)
+
+
+def make_protobuf():
+    print("protobuf installing...")
+    run_command(["sudo", "apt-get", "install", "automake", "libtool", 
+                 "autoconf", "curl", "make", "g++", "unzip", "-y"])
+    
+    PROTOBUF = "protobuf-3.21.11"
+    install_path = os.path.join(current_path)
+    os.chdir(install_path)
+
+    file = os.path.join(install_path, f"{PROTOBUF}.zip")
+    if not os.path.exists(file):
+        print(f"{os.path.basename(file)} not found")
+        sys.exit(1)
+        
+    with zipfile.ZipFile(file, "r") as zip_ref:
+        zip_ref.extractall(install_path)
+    
+    build_path = os.path.join(install_path, PROTOBUF)
+    os.chdir(build_path)
+
+    configure = os.path.join(build_path, "configure")
+
+    run_command(["chmod", "+x", configure])
+    run_command([configure, f"--prefix={install_path}/protobuf"])
+    run_command(["make"])
+    # run_command(["make", "check"])
+    run_command(["make", "install"])
+
+    print("protobuf completed")
+
+if __name__ == "__main__":
+    make_protobuf()
---
+++ message/generate.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+SRC_DIR=./
+DST_DIR=../protocal
+
+../protobuf/bin/protoc -I=$SRC_DIR --cpp_out=$DST_DIR/ $SRC_DIR/*.proto
